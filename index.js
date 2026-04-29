fc128827be109471b28401cac03fe47148346aee
spearsjonesco-coder <spearsjonesco@gmail.com>
2026-04-29 15:58:23 -0400

    Mod5Present

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
 // API 1: "https://jsonplaceholder.typicode.com/users"
 // API 2: "https://jsonplaceholder.typicode.com/posts?userId=:id"
+const userListEl = document.querySelector(".user-list");
 
 async function main() {
-const users = await fetch("https://jsonplaceholder.typicode.com/users");
-const usersData = await users.json();
-const userListEl = document.querySelector(".user-list");
-console.log(usersData);
-userListEl.innerHTML = usersData.map((user) => usersHTML(user)).join("");
+   const users = await fetch("https://jsonplaceholder.typicode.com/users");
+   const usersData = await users.json();
+   userListEl.innerHTML = usersData.map((user) => usersHTML(user)).join("");
 }
 
 main();
 
 function userHTML(user) {
   return `<div class="user-card">
-<div class="user-card__container">
+  <div class="user-card__container">
   <h3>${user.name}</h3>
     <p><b>Email:</b> ${user.email}</p>
     <p><b>Phone:</b> ${user.phone}</p>
     <p><b>Website:</b> <a href="https://${user.website}" target="_blank">${user.website}</a></p>
   </div>
-</div>`;
+</div>
+`;
  }
