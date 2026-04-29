dac9bf7b38164dbc7b6b5c6217a1de23603b995a
spearsjonesco-coder <spearsjonesco@gmail.com>
2026-04-29 14:24:52 -0400

    Module-5

--- index.js
+++ index.js
@@ -4,11 +4,12 @@
 async function main() {
 const users = await fetch("https://jsonplaceholder.typicode.com/users");
 const usersData = await users.json();
-console.log
-usersData
-  .map(
-    (user) => <div class="user-card">
-<div class="user-card__container">
+console.log(usersData);
+
+const usersHTML = usersData
+  .map((user) => (
+  <div class="user-card">
+    <div class="user-card__container">
        <h3>User's Name</h3>
          <p><b>Email:</b> email@email.com</p>
          <p><b>Phone:</b> 0000000000</p>
