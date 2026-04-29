33d1cf7704f8e64b81bae03b75a723414ab289b2
spearsjonesco-coder <spearsjonesco@gmail.com>
2026-04-29 15:43:03 -0400

    Mod5Present

--- index.js
+++ index.js
@@ -4,26 +4,20 @@
 async function main() {
 const users = await fetch("https://jsonplaceholder.typicode.com/users");
 const usersData = await users.json();
+const userListEl = document.querySelector(".user-list");
 console.log(usersData);
-
-const usersHTML = usersData
-  .map((user) => {
-    console.log(user);
-
-    return
-      <div class="user-card">
-        <div class="user-card__container">
-         <h3>${user.name}</h3>
-         <p><b>Email:</b> ${user.email}</p>
-         <p><b>Phone:</b> ${user.phone}</p>
-         <p><b>Website:</b> <a href="https://${user.website}" target="_blank">${user.website}</a></p>
-     </div>
-  </div>
-  ;
-  })
-  .join("");
-
-  document.body.innerHTML = usersHTML;
+userListEl.innerHTML = usersData.map((user) => usersHTML(user)).join("");
 }
 
 main();
+
+function userHTML(user) {
+  return `<div class="user-card">
+<div class="user-card__container">
+  <h3>${user.name}</h3>
+    <p><b>Email:</b> ${user.email}</p>
+    <p><b>Phone:</b> ${user.phone}</p>
+    <p><b>Website:</b> <a href="https://${user.website}" target="_blank">${user.website}</a></p>
+  </div>
+</div>`;
+ }
